4afd79a76dcff81e673384b8772671140aef87f5
Alii <FLORIDAshabana@users.noreply.github.com>
2019-07-26 16:21:04 -0700

    modified

--- app/src/main/java/com/example/florida/easydeal0th/StartActivity.java
+++ app/src/main/java/com/example/florida/easydeal0th/StartActivity.java
@@ -2,12 +2,16 @@ package com.example.florida.easydeal0th;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button ALI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
     }
 }
